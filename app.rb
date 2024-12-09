e54390d9e5da4ac361509d1f96501717c39db895
Hasse20 <hansson06.eric@gmail.com>
2024-12-09 14:22:32 +0100

    seeder
    fixade seeder så nu funkar båda sidorna i front end och databasen sparar ett id och tre vikter

--- app.rb
+++ app.rb
@@ -1,7 +1,78 @@
 class App < Sinatra::Base
 
-    get '/' do
+    get '/index' do
         erb(:"index")
     end
+    get '/program' do
+        erb(:"program")
+    end
 
+        # Funktion för att prata med databasen
+    # Exempel på användning: db.execute('SELECT * FROM fruits')
+    def db
+        return @db if @db
+
+        @db = SQLite3::Database.new("db/lifts.sqlite")
+        @db.results_as_hash = true
+
+        return @db
+      end
+
+      # Routen gör en redirect till '/lifts'
+      get '/' do
+          redirect('/lifts')
+      end
+
+      #Routen hämtar alla övningar i databasen
+      get '/lifts' do
+        @lifts = db.execute('SELECT * FROM lifts')
+        p @lifts
+        erb(:"lifts/index")
+      end
+
+      # Routen visar ett formulär för att spara ett nytt lyft till databasen.
+      get '/lifts/new' do
+        erb(:"lifts/new")
+      end
+
+      # Routen sparar ett lyft till databasen och gör en redirect till '/lifts'.
+      post '/lifts' do
+        p params
+        name = params["fruit_name"]
+        description = params["fruit_description"]
+
+        db.execute("INSERT INTO fruits (name, description) VALUES(?,?)", [name, description])
+
+        redirect("/fruits")
+      end
+
+      # Routen visar all info (från databasen) om en frukt
+      get '/fruits/:id' do | id |
+        @fruit = db.execute('SELECT * FROM fruits WHERE id=?',id).first
+        erb(:"fruits/show")
+      end
+
+      # Routen tar bort frukten med id
+      post '/fruits/:id/delete' do | id |
+        db.execute("DELETE FROM fruits WHERE id =?", id)
+        redirect("/fruits")
+      end
+
+      # Routen visar ett formulär på edit.erb för att ändra frukten med id
+      get '/fruits/:id/edit' do | id |
+        # todo: Hämta info (från databasen) om frukten med id
+
+        # todo: Visa infon i fruits/edit.erb
+      end
+
+      # Routen sparar ändringarna från formuläret
+      post "/fruits/:id/update" do | id |
+        # todo: Läs name & category från formuläret
+
+        # todo: Kör SQL för att uppdatera datan från formuläret
+
+        # todo: Redirect till /fruits
+      end
+
+  end
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -3,9 +3,35 @@ require 'sqlite3'
 class Seeder
 
   def self.seed!
-    p "doit"
+    drop_tables
+    create_tables
+    populate_tables
   end
 
+  def self.drop_tables
+    db.execute('DROP TABLE IF EXISTS lifts')
+  end
+
+  def self.create_tables
+    db.execute('CREATE TABLE lifts (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                deadlift FLOAT,
+                shoulderpress FLOAT,
+                benchpress FLOAT)')
+  end
+  def self.populate_tables
+    db.execute('INSERT INTO lifts (deadlift, shoulderpress, benchpress) VALUES (40, 20, 30)')
+  end
+
+  private
+  def self.db
+    return @db if @db
+    @db = SQLite3::Database.new('db/fit5x.sqlite')
+    @db.results_as_hash = true
+    @db
+  end
 end
+
+
 
 Seeder.seed!
